56cc10a799bd1caf32ef9e022027e92c756b5e8f
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2021-01-30 14:19:11 -0400

    login & registration page templates

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from '../layouts/auth-layout/auth-layout.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
     {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
+            }
+        ]
     }
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LayoutsModule } from '../layouts/layouts.module';
 
 
 @NgModule({
     declarations: [LoginComponent, RegisterComponent],
     imports: [
         CommonModule,
+        LayoutsModule,
         AuthRoutingModule
     ]
 })
