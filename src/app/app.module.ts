d47d63cab51d86a0b7f1ebfe6a3891ef81c2e8fb
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2021-02-07 00:58:11 -0400

    login flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PagesModule } from './pages/pages.module';
+import { APP_LOCAL_STORAGE, APP_SESSION_STORAGE } from './tokens';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,16 @@ import { PagesModule } from './pages/pages.module';
         PagesModule,
         AppRoutingModule
     ],
-    providers: [],
+    providers: [
+        {
+            provide: APP_SESSION_STORAGE,
+            useValue: window.sessionStorage
+        },
+        {
+            provide: APP_LOCAL_STORAGE,
+            useValue: window.localStorage
+        }
+    ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule {
